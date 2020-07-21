7ae4343b4816ba3f1d866ca3cbaf8d26c2136735
Piyush Garg <piyushgarg001@gmail.com>
2020-07-21 18:43:49 +0530

    Fix script

--- task/jib-maven/0.1/tests/pre-apply-task-hook.sh
+++ task/jib-maven/0.1/tests/pre-apply-task-hook.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 # Add an internal registry as sidecar to the task so we can upload it directly
 # from our tests withouth having to go to an external registry.
--- task/maven/0.1/tests/pre-apply-task-hook.sh
+++ task/maven/0.1/tests/pre-apply-task-hook.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 # Add git-clone
 kubectl -n ${tns} apply -f ./task/git-clone/0.1/git-clone.yaml
